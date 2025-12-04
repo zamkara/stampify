ef54d6f83b1a0e2a362425abd95a558f9ccb10e5
zamkara <zamkara@gnuweeb.org>
2025-12-04 20:07:05 +0700

    y

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest, NextResponse } from "next/server";
-export const runtime = "nodejs";
+export const runtime = "edge";
 
 const USER_AGENT =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
